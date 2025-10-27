db93e74293765b247cddb41d2a524fa0c735919d
PedroGTG <ph.xavier.exe@gmail.com>
2025-10-26 22:11:38 -0300

    feat: adiciona feature de reordenamento

--- logica_de_missao/pi1/interfaces/comandos.ts
+++ logica_de_missao/pi1/interfaces/comandos.ts
@@ -2,20 +2,23 @@
 export type comandos_aceitos = "Andar" | "Virar" | "Largar"
 
 interface Comando {
+  id: string;
   tipo: comandos_aceitos
 }
 
 interface Andar extends Comando {
+  tipo: "Andar";
   distancia: number;
   
 }
 
 interface Virar extends Comando {
+  tipo: "Virar";
   lado: "Direita" | "Esquerda"
 }
 
 interface Largar extends Comando {
-
+  tipo: "Largar";
 }
 
 export type { Comando, Andar, Virar, Largar}
